eed7152d1b8f9e1c51194c02b4f3aab99c3153fe
akmaljonmordayev <akmaljonmordayev@gmail.com>
2026-03-31 16:15:34 +0500

    done some chnages

---
+++ src/components/ui/BlogerCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BlogerCard() {
+  return (
+    <div>BlogerCard</div>
+  )
+}
+
+export default BlogerCard
---
+++ src/components/ui/ReklamaModal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ReklamaModal() {
+  return (
+    <div>ReklamaModal</div>
+  )
+}
+
+export default ReklamaModal
